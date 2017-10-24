e95ac4267bb2ac179fc331a75203e18cac88cf5a
Maya Driver <mayadrivermd@gmail.com>
2017-10-24 20:52:43 +0100

    more nested object work

--- src/main/scala/JaycenParser.scala
+++ src/main/scala/JaycenParser.scala
@@ -3,24 +3,26 @@ import Validations._
 
 object JaycenParser {
 
-  def parse[T](rawJson: String): Jaycen[T] =
+  def parse[T](rawJson: String) =
     isValidJson(rawJson) match {
       case InvalidJaycen(_, error) => throw new Exception(error)
       case ValidJaycen(json) =>
-        val keyValuePairs: List[String] = sanitizedString(json).split(",").toList
+        val keyValuePairs: List[String] = curlyBraceRemover(json).split(",").toList
 
-        println(keyValuePairs.map(p => p.split(":").toList))
+        val rawJayObjects = keyValuePairs.map(p => p.split(":").toList)
 
+        println(rawJayObjects)
         Jaycen(toJaycenFields(keyValuePairs))
     }
 
-  private def toJaycenFields[T](pairs: List[String]): List[JayObject] =
-    pairs.flatMap{ pair =>
+  private def toJaycenFields[T](pairs: List[String]): List[JayObject] = {
+    pairs.flatMap { pair =>
       pair.split(":").toList match {
         case h :: m :: t => JayObject(JayField(quoteMarkRemover(h)), toJayValue(quoteMarkRemover(m))) :: toJaycenFields(t)
         case _ => throw new Exception("invalid jaycen")
       }
     }
+  }
 
   private def toJayValue(raw: String): JayValue =
     if (isArray(raw)) JayArray(squareBracketRemover(raw).split(",").toList)
